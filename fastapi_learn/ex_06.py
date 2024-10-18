01fab2e2013d7e1aea667d019991a785b74e8863
lexxai <lexxai@gmail.com>
2024-10-18 19:54:57 +0300

    ex_06

---
+++ fastapi_learn/ex_06.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+async def dependency_with_yield():
+    # Setup code: this runs before the route
+    print("Setting up the resource")
+    resource = "Resource is ready"
+    yield resource  # Route will receive this value
+    
+    # Teardown code: this runs after the route completes
+    print("Cleaning up the resource")
+
+@app.get("/example_06")
+async def read_example(resource: str = Depends(dependency_with_yield)):
+    print("Handling request with resource:", resource)
+    return {"message": "Request handled"}
