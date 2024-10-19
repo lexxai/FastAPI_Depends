77937199359f15254d72d62a37c03d48154ab03f
lexxai <lexxai@gmail.com>
2024-10-19 20:16:59 +0300

    ex_23

---
+++ fastapi_learn/ex_23.py
@@ -0,0 +1,14 @@
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+
+def some_dependency():
+    return "dependency result"
+
+
+def some(in_data: str, dep: str = Depends(some_dependency)):
+    print(f"Input: {in_data}, Dependency: {dep}")
+
+
+some("aaa")  # Dependency will be run as function here
