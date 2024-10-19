0fbe4d3a8c191c39d74496b2510a69ed739e7d42
lexxai <lexxai@gmail.com>
2024-10-19 20:16:53 +0300

    ex_22

---
+++ fastapi_learn/ex_22.py
@@ -0,0 +1,14 @@
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+def some_dependency():
+    return "dependency result"
+
+def some(in_data: str, dep: str = Depends(some_dependency)):
+    print(f"Input: {in_data}, Dependency: {dep}")
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    some("aaa")  # Dependency will be injected here
+    return {"item_id": item_id}
