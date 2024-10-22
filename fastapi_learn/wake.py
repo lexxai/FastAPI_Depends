6938298329b71a322b9dc64290e27ea6d96bff1e
lexxai <lexxai@gmail.com>
2024-10-22 16:32:34 +0300

    # Wait before the next ping (e.g., 300 seconds + random shift)

--- fastapi_learn/wake.py
+++ fastapi_learn/wake.py
@@ -1,17 +1,20 @@
 import os
 import asyncio
+import random
 import httpx
 
 
 # Async function to perform the periodic ping
 async def ping_service():
     url = os.getenv("PING_URL", "https://render.com")
-    delay = int(os.getenv("PING_DELAY", 60))  # Default delay is 10 seconds
+    delay = int(os.getenv("PING_DELAY", 300))  # Default delay is 5 mins
     while True:
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.head(url)  # Async HTTP request
-                if response.status_code != 200:
+                if response.status_code == 200:
+                    print(f"ping_service state OK")
+                else:
                     print(
                         f"{__name__} ping_service to {url}. Service returned status: {response.status_code}"
                     )
@@ -19,8 +22,8 @@ async def ping_service():
             print(f"Error while pinging service: {e}")
 
         try:
-            # Wait before the next ping (e.g., 60 seconds)
-            await asyncio.sleep(delay)
+            # Wait before the next ping (e.g., 300 seconds + random shift)
+            await asyncio.sleep(delay + random.uniform(0, delay // 2))
         except asyncio.exceptions.CancelledError:
             print("Ping service was cancelled.")
             break  # Exit loop and terminate task gracefully
