d981f68a6c77ab5c5c74b0fb17910d1d8a042f35
lexxai <lexxai@gmail.com>
2024-10-22 20:55:10 +0300

    - su

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt install -y postgresql postgresql-contrib
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
+apt update
+apt install -y postgresql postgresql-contrib
+systemctl start postgresql
+systemctl enable postgresql
+
+docker -v
 
 cd fastapi_learn
 fastapi run main.py
